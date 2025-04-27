5c8950f16759bb94723696acb42c11f94acf981b
Brianna Jackson <bjackson59@student.cscc.edu>
2025-04-27 15:24:46 -0400

    Changed the line width and color. Changed the size and color of the tick values on the x and y.

--- sine_wave_graph.py
+++ sine_wave_graph.py
@@ -9,7 +9,8 @@ time = np.arange(0, 3.0, 0.01)
 air_pressure = 0.5 + np.sin(3.0 * time)
 # y = sin(x)
 
-plt.plot(time, air_pressure)
+plt.plot(time, air_pressure, linewidth=2, color='green')
+plt.tick_params(labelsize=10, color='green')
 
 plt.title('Air Pressure Over Time', fontsize=15)
 plt.xlabel('Time (seconds)', fontsize=13)
